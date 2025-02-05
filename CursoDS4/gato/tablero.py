4becddcdc9e7d34d08c68ae2790652a3f5269ea6
DaRonin13 <bryangaso@hotmail.es>
2025-02-05 10:56:34 -0700

    Prueba

--- CursoDS4/gato/tablero.py
+++ CursoDS4/gato/tablero.py
@@ -18,7 +18,7 @@ def ia(simbolos:dict):
     ocupado = True
     while ocupado == True:
         x = random.choice(list(simbolos.keys()))
-        if simbolos[x] not in ['X','O']:
+        if simbolos(x) not in ['X','O']:
             simbolos[x] = 'O'
             ocupado = False
 
@@ -37,33 +37,73 @@ def usuario(simbolos:dict):
         else:
             print('Número incorrecto')
 
+
+def juego (simbolos:dict):
+    '''Juego del gato'''
+
+    lista_combinaciones = [
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['3','6','9'],
+    ]
+    en_juego=True
+    ganador=""
+    movimientos = 0
+    dibuja_tablero(simbolos)
+    movimientos = 0
+    gana = None
+
+    while en_juego:
+        if movimientos < 9:
+            usuario(simbolos)
+            dibuja_tablero(simbolos)
+            movimientos +=1
+            gana = checa_winner(simbolos,lista_combinaciones)
+            if gana is not None:
+                en_juego=False
+                continue
+            if movimientos >=9:
+                en_juego=False
+                continue
+            ia(simbolos)
+            dibuja_tablero(simbolos)
+            movimientos+=1
+            gana = checa_winner(simbolos,lista_combinaciones)
+            if gana is not None:
+                en_juego=False
+                continue
+            if movimientos >=9:
+                en_juego=False
+                continue
+    return gana
+
+
+def checa_winner(simbolos:dict, combinaciones:list):
+    '''Checa si hay un ganador'''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos [c[2]]:
+            return simbolos[c[0]]
+    return None
+
 if __name__ == '__main__':
     numeros = [str(x) for x in range(1,10)]
     dsimbolos = {x:x for x in numeros}
+    g = juego(dsimbolos)
+    if g is not None:
+        print(f'El ganador es {g}')
+    else:
+        print('Empate')
+        
+    '''
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
     dibuja_tablero(dsimbolos)
     usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
-
-    def juego (simbolos:dict):
-        '''Juego del gato'''
-
-        lista_6combinaciones = [
-            ['1','2','3'],
-            ['4','5','6'],
-            ['7','8','9'],
-            ['1','4','7'],
-            ['2','5','8'],
-            ['3','6','9'],
-        ]
-
-        def checa_winner(simbolos:dict, combinaciones:list):
-            '''Checa si hay un ganador'''
-            for c in combinaciones:
-                if simbolos[c[0]] == simbolos[c[1]] == simbolos [c[2]]:
-                    return simbolos[combinacion[0]]
-                return None
+    '''
 
     '''
     x  = random.choice(numeros)
